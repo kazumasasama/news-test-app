a2c677f6c1c0ed3bac545526ee0a796dec1003cc
Kaz <takaik@yahoo.com>
2022-03-29 11:21:23 -0400

    hide api key

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,6 @@
  class NewsController < ApplicationController 
   def index
-    res = HTTP.get("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=cf4dd2b6995d455a96505fbeb59d415e")
+    res = HTTP.get("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=#{Rails.application.credentials.news_api_key}")
     articles = res.parse(:json)['articles']
     render json: articles
   end
